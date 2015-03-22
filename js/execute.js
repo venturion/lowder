7adabb8982ad0ed858346447e1c5b251ec90d3d6
Felipe Venturino <felipeventurino@hotmail.com>
2015-03-22 18:01:23 -0300

    comentando options

---
+++ js/execute.js
@@ -0,0 +1,14 @@
+
+//Video
+var elm = document.querySelectorAll('video');
+
+for (var i = 0; i < elm.length; i++) {
+  elm[i].muted = true;
+}
+
+//Audio
+var elm = document.querySelectorAll('audio');
+
+for (var i = 0; i < elm.length; i++) {
+  elm[i].muted = true;
+}
